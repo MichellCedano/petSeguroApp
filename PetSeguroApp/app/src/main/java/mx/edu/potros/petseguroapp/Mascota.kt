ae9e9ba7e3d52a070cd45bfa32c0c721870f685b
amse1029 <alexa.soto236348@potros.itson.edu.mx>
2024-04-18 15:04:37 -0700

    mascota activity

--- PetSeguroApp/app/src/main/java/mx/edu/potros/petseguroapp/Mascota.kt
+++ PetSeguroApp/app/src/main/java/mx/edu/potros/petseguroapp/Mascota.kt
@@ -1,11 +1,26 @@
 package mx.edu.potros.petseguroapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Mascota : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mascota)
+
+        val buttonAgendar : Button = findViewById(R.id.btnAgendar)
+        val buttonRegresar : Button = findViewById(R.id.btnRegresar)
+
+        buttonRegresar.setOnClickListener {
+            var intent: Intent = Intent( this, MisMascotasActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonAgendar.setOnClickListener {
+            var intent: Intent = Intent( this, AgendarCitaActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
