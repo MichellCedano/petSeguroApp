9882f97164b80e2977ed35d3b7e76845ae3d2e24
MichellCedano <michell.cedano233230@potros.itson.edu.mx>
2024-04-18 02:29:55 -0700

    Arregle lo de mis mascotas y el agendar una cita

--- PetSeguroApp/app/src/main/java/mx/edu/potros/petseguroapp/AgendarCitaActivity.kt
+++ PetSeguroApp/app/src/main/java/mx/edu/potros/petseguroapp/AgendarCitaActivity.kt
@@ -3,8 +3,10 @@ package mx.edu.potros.petseguroapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Spinner
 
 class AgendarCitaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +19,19 @@ class AgendarCitaActivity : AppCompatActivity() {
             var intent: Intent = Intent( this, Mascota::class.java)
             startActivity(intent)
         }
+
+        val spinner: Spinner = findViewById(R.id.servicios_spinner)
+// Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.servicios_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears.
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner.
+            spinner.adapter = adapter
+        }
+
     }
 }
