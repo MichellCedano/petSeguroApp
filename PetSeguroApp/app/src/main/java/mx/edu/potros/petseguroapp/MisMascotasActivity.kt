a3ee05db8579644ede6dce9d044b3708ba911e3a
amse1029 <alexa.soto236348@potros.itson.edu.mx>
2024-04-18 00:09:56 -0700

    layout mis mascotas, mascota, registrar mascota, agendar cita mascota

--- PetSeguroApp/app/src/main/java/mx/edu/potros/petseguroapp/MisMascotasActivity.kt
+++ PetSeguroApp/app/src/main/java/mx/edu/potros/petseguroapp/MisMascotasActivity.kt
@@ -1,11 +1,50 @@
 package mx.edu.potros.petseguroapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class MisMascotasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mis_mascotas)
+
+        val buttonRegistrar : Button = findViewById(R.id.btnRegistroMascota)
+
+        val buttonRegresar : Button = findViewById(R.id.btnRegresar)
+
+        val butttonMascota1 : ImageButton = findViewById(R.id.ibMascota1)
+        val butttonMascota2 : ImageButton = findViewById(R.id.ibMascota2)
+        val butttonMascota3 : ImageButton = findViewById(R.id.ibMascota3)
+        val butttonMascota4 : ImageButton = findViewById(R.id.ibMascota4)
+
+        buttonRegistrar.setOnClickListener {
+            var intent: Intent = Intent( this, RegistroMascota::class.java)
+            startActivity(intent)
+        }
+
+        buttonRegresar.setOnClickListener {
+            var intent: Intent = Intent( this, MenuActivity::class.java)
+            startActivity(intent)
+        }
+
+        butttonMascota1.setOnClickListener {
+            var intent: Intent = Intent( this, Mascota::class.java)
+            startActivity(intent)
+        }
+        butttonMascota2.setOnClickListener {
+            var intent: Intent = Intent( this, Mascota::class.java)
+            startActivity(intent)
+        }
+        butttonMascota3.setOnClickListener {
+            var intent: Intent = Intent( this, Mascota::class.java)
+            startActivity(intent)
+        }
+        butttonMascota4.setOnClickListener {
+            var intent: Intent = Intent( this, Mascota::class.java)
+            startActivity(intent)
+        }
     }
 }
--- PetSeguroApp/app/src/main/java/mx/edu/potros/petseguroapp/RegistroMascota.kt
+++ PetSeguroApp/app/src/main/java/mx/edu/potros/petseguroapp/RegistroMascota.kt
@@ -4,17 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageButton
 
-class AgendarCitaActivity : AppCompatActivity() {
+class RegistroMascota : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_agendar_cita)
+        setContentView(R.layout.activity_registro_mascota)
+
+        val buttonRegistrar : Button = findViewById(R.id.btnRegistrar)
 
         val buttonRegresar : Button = findViewById(R.id.btnRegresar)
 
+        buttonRegistrar.setOnClickListener {
+            var intent: Intent = Intent( this, RegistroMascota::class.java)
+            startActivity(intent)
+        }
+
         buttonRegresar.setOnClickListener {
-            var intent: Intent = Intent( this, Mascota::class.java)
+            var intent: Intent = Intent( this, MisMascotasActivity::class.java)
             startActivity(intent)
         }
     }
