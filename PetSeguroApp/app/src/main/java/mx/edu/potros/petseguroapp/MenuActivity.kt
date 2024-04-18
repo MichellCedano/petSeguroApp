f664f9da9e3cfeac3f12fb412d0213f798c93f5c
MichellCedano <michell.cedano233230@potros.itson.edu.mx>
2024-04-17 20:44:56 -0700

    Añadi el menu y cada una de las pantallas para las secciones, falta hacer cada sección

--- PetSeguroApp/app/src/main/java/mx/edu/potros/petseguroapp/MenuActivity.kt
+++ PetSeguroApp/app/src/main/java/mx/edu/potros/petseguroapp/MenuActivity.kt
@@ -1,11 +1,58 @@
 package mx.edu.potros.petseguroapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class MenuActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        val buttonCalendario : ImageButton = findViewById(R.id.ibCalendario)
+
+        val buttonPreguntasFrecuentes : ImageButton = findViewById(R.id.ibPreguntasFrec)
+
+        val buttonMisMascotas : ImageButton = findViewById(R.id.ibMisMascotas)
+
+        val buttonEntrenamiento : ImageButton = findViewById(R.id.ibEntrenamiento)
+
+        val buttonAgendarCita : ImageButton = findViewById(R.id.ibAgendarCita)
+
+        val buttonCerrarSesion : ImageButton = findViewById(R.id.ibCerrarSesion)
+
+        buttonCalendario.setOnClickListener {
+            var intent: Intent = Intent( this, CalendarioActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonPreguntasFrecuentes.setOnClickListener {
+            var intent: Intent = Intent( this, PreguntasFrecActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonMisMascotas.setOnClickListener {
+            var intent: Intent = Intent( this, MisMascotasActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonEntrenamiento.setOnClickListener {
+            var intent: Intent = Intent( this, EntrenamientoActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonAgendarCita.setOnClickListener {
+            var intent: Intent = Intent( this, AgendarCitaActivity::class.java)
+            startActivity(intent)
+        }
+        buttonCerrarSesion.setOnClickListener {
+            var intent: Intent = Intent( this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- PetSeguroApp/app/src/main/java/mx/edu/potros/petseguroapp/SignUpActivity.kt
+++ PetSeguroApp/app/src/main/java/mx/edu/potros/petseguroapp/SignUpActivity.kt
@@ -1,11 +1,21 @@
 package mx.edu.potros.petseguroapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        val buttonRegistro : Button = findViewById(R.id.btnregistrarse)
+
+        buttonRegistro.setOnClickListener {
+            var intent: Intent = Intent( this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
