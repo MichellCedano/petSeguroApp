c04c75075c32c230b179605da55ec76c09df426e
arrcristian <cristian.arreola233140@potros.itson.edu.mx>
2024-05-07 22:52:58 -0700

    Entrenamiento listo

--- PetSeguroApp/app/src/main/java/mx/edu/potros/petseguroapp/EntrenamientoActivity.kt
+++ PetSeguroApp/app/src/main/java/mx/edu/potros/petseguroapp/EntrenamientoActivity.kt
@@ -4,18 +4,51 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class EntrenamientoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_entrenamiento)
 
-        val buttonRegresar : Button = findViewById(R.id.btnRegresar)
+        val tvTitulo = findViewById<TextView>(R.id.tvTituloEntrenamiento)
 
-
+        val buttonRegresar: Button = findViewById(R.id.btnRegresar)
         buttonRegresar.setOnClickListener {
-            var intent: Intent = Intent( this, MenuActivity::class.java)
-            startActivity(intent)
+            finish()
+        }
+
+        // Recibir el nombre del entrenamiento seleccionado del intent
+        val entrenamiento = intent.getStringExtra("entrenamiento")
+
+        // Configurar la actividad para mostrar el entrenamiento correspondiente
+        when (entrenamiento) {
+            "Sentarse" -> {
+                tvTitulo.text = "Sentarse"
+                val imageView = findViewById<ImageView>(R.id.imageView)
+                val tvInstrucciones = findViewById<TextView>(R.id.tvInstrucciones)
+
+                imageView.setImageResource(R.drawable.entrenamiento_sentado)
+                tvInstrucciones.text = getString(R.string.entrenamiento_1)
+            }
+            "Ponerse a dos patas" -> {
+                tvTitulo.text = "Ponerse a dos patas"
+                val imageView = findViewById<ImageView>(R.id.imageView)
+                val tvInstrucciones = findViewById<TextView>(R.id.tvInstrucciones)
+
+                imageView.setImageResource(R.drawable.entrenamiento_dos_patas)
+                tvInstrucciones.text = getString(R.string.entrenamiento_2)
+            }
+            "Dar la pata" -> {
+                tvTitulo.text = "Dar la pata"
+                val imageView = findViewById<ImageView>(R.id.imageView)
+                val tvInstrucciones = findViewById<TextView>(R.id.tvInstrucciones)
+
+                imageView.setImageResource(R.drawable.entrenamiento_pata)
+                tvInstrucciones.text = getString(R.string.entrenamiento_3)
+            }
+
         }
     }
 }
--- PetSeguroApp/app/src/main/java/mx/edu/potros/petseguroapp/MenuEntrenamientoActivity.kt
+++ PetSeguroApp/app/src/main/java/mx/edu/potros/petseguroapp/MenuEntrenamientoActivity.kt
@@ -10,28 +10,32 @@ class MenuEntrenamientoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_entrenamiento)
 
-        val buttonRegresar : Button = findViewById(R.id.btnRegresar)
-
-        val butttonEntrenamiento1 : Button = findViewById(R.id.btnSentarse)
-        val butttonEntrenamiento2 : Button = findViewById(R.id.btnPonerseADosPatas)
-        val butttonEntrenamiento3 : Button = findViewById(R.id.btnDarLaPata)
+        val buttonRegresar: Button = findViewById(R.id.btnRegresar)
+        val buttonSentarse: Button = findViewById(R.id.btnSentarse)
+        val buttonPonerseADosPatas: Button = findViewById(R.id.btnPonerseADosPatas)
+        val buttonDarLaPata: Button = findViewById(R.id.btnDarLaPata)
 
         buttonRegresar.setOnClickListener {
-            var intent: Intent = Intent( this, MenuActivity::class.java)
+            val intent = Intent(this, MenuActivity::class.java)
             startActivity(intent)
         }
 
-        butttonEntrenamiento1.setOnClickListener {
-            var intent: Intent = Intent( this, EntrenamientoActivity::class.java)
-            startActivity(intent)
+        buttonSentarse.setOnClickListener {
+            abrirEntrenamientoActivity("Sentarse")
         }
-        butttonEntrenamiento2.setOnClickListener {
-            var intent: Intent = Intent( this, EntrenamientoActivity::class.java)
-            startActivity(intent)
+
+        buttonPonerseADosPatas.setOnClickListener {
+            abrirEntrenamientoActivity("Ponerse a dos patas")
         }
-        butttonEntrenamiento3.setOnClickListener {
-            var intent: Intent = Intent( this, EntrenamientoActivity::class.java)
-            startActivity(intent)
+
+        buttonDarLaPata.setOnClickListener {
+            abrirEntrenamientoActivity("Dar la pata")
         }
+    }
+
+    private fun abrirEntrenamientoActivity(entrenamiento: String) {
+        val intent = Intent(this, EntrenamientoActivity::class.java)
+        intent.putExtra("entrenamiento", entrenamiento)
+        startActivity(intent)
     }
 }
